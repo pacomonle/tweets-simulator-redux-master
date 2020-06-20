b24f5c4336ef6ee3ceb06ef155332d4a22603599
pacomonle <franciscomonleonleal@gmail.com>
2020-06-20 21:14:19 +0200

    build

---
+++ build/precache-manifest.a623a57442940a969db5bb9a20874ebc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650c19bace157a7dc4d114f1a4549a81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "364ce1ea5a13af7f6ecb",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8e2da3aaed87e59e262c",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "364ce1ea5a13af7f6ecb",
+    "url": "/static/js/2.a85dc62a.chunk.js"
+  },
+  {
+    "revision": "8e2da3aaed87e59e262c",
+    "url": "/static/js/main.07e08e3c.chunk.js"
+  },
+  {
+    "revision": "ceb3df220f5e6d118451",
+    "url": "/static/js/runtime-main.4d8d8316.js"
+  },
+  {
+    "revision": "8251fe76d7487a28b07603515b300964",
+    "url": "/static/media/redux.8251fe76.png"
+  }
+]);
